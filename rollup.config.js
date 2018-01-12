6a15766ef1d14285b12cd1432c999c9d3a48c96b
Gianluca Guarini <gianluca.guarini@gmail.com>
2018-01-12 21:15:02 +0100

    updated: remove the typescript setup

--- rollup.config.js
+++ rollup.config.js
@@ -1,45 +1,9 @@
-/*
-  This config will output this files:
+import resolve from 'rollup-plugin-node-resolve';
 
-  ./dist/tag-parser.js  -> Factory for TagParser (the default export)
-  ./dist/skip-es6-tl    -> skipES6TL function
-  ./dist/skip-regex     -> skipRegex function
-  ./nodeTypes           -> the NodeTypes enum
+export default {
+  input: 'lib/src/index.js',
+  dest: './index.js',
+  format: 'cjs',
+  plugins: [resolve()]
+}
 
-*/
-import alias   from 'rollup-plugin-alias'
-import cleanup from 'rollup-plugin-cleanup'
-
-export default [
-  {
-    entry: 'lib/node-types.js',
-    dest: './nodeTypes.js',
-    format: 'cjs'
-  },
-  {
-    entry: 'lib/skip-es6-tl.js',
-    dest: './dist/skip-es6-tl.js',
-    format: 'cjs',
-    plugins: [
-      cleanup()
-    ]
-  },
-  {
-    entry: 'lib/tag-parser.js',
-    dest: 'dist/tag-parser.js',
-    format: 'cjs',
-    interop: false,
-    plugins: [
-      alias({
-        resolve: ['.js'],
-        './skip-es6-tl': './../src/proxys/skip-es6-tl.js',
-        './skip-regex': './../src/proxys/skip-regex.js'
-      }),
-      cleanup()
-    ],
-    globals: {
-      'skip-es6-tl': 'skipES6TL',
-      'skip-regex': 'skipRegex'
-    }
-  }
-]
